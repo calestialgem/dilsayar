1753ee56050aae9032be7714d24d06e2703af14e
Cem GEÇGEL <gecgelcem@outlook.com>
2022-07-27 19:18:16 +0300

    remove: Parser Implicit Terminals

--- src/dil/parser.c
+++ src/dil/parser.c
@@ -132,148 +132,115 @@ void dil_parse__error_set(DilParseContext* context, DilString const* set)
     dil_parse__error(context, buffer);
 }
 
-/* Try to parse a specific terminal. */
-bool dil_parse__terminal(DilParseContext* context, char terminal)
-{
-    dil_parse__create(DIL_SYMBOL_TERMINAL, false);
-    dil_parse__return(dil_string_prefix_element(context->string, terminal));
-}
-
-/* Try to parse a set terminal. */
-bool dil_parse__terminal_set(DilParseContext* context, DilString const* set)
-{
-    dil_parse__create(DIL_SYMBOL_TERMINAL, false);
-    dil_parse__return(dil_string_prefix_set(context->string, set));
-}
-
-/* Try to parse a not set terminal. */
-bool dil_parse__terminal_not_set(DilParseContext* context, DilString const* set)
-{
-    dil_parse__create(DIL_SYMBOL_TERMINAL, false);
-    dil_parse__return(dil_string_prefix_not_set(context->string, set));
-}
-
-/* Try to parse a string of terminals. */
-bool dil_parse__terminal_string(
-    DilParseContext* context,
-    DilString const* terminals)
-{
-    dil_parse__create(DIL_SYMBOL_TERMINAL, false);
-    dil_parse__return(dil_string_prefix_check(context->string, terminals));
-}
-
 /* Try to parse an identifier. */
 bool dil_parse_identifier(DilParseContext* context)
 {
-    dil_parse__create(DIL_SYMBOL_IDENTIFIER, false);
+    dil_parse__create(DIL_SYMBOL_IDENTIFIER, true);
 
     DilString const SET_0 = dil_string_terminated("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     DilString const SET_1 = dil_string_terminated(
         "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
 
-    if (!dil_parse__terminal_set(context, &SET_0)) {
+    if (!dil_string_prefix_set(context->string, &SET_0)) {
         dil_parse__return(false);
     }
 
-    while (dil_parse__terminal_set(context, &SET_1)) {}
+    while (dil_string_prefix_set(context->string, &SET_1)) {}
 
     dil_parse__return(true);
 }
 
 /* Try to parse an escaped character. */
-bool dil_parse_escaped__terminal(DilParseContext* context)
+bool dil_parse_escaped(DilParseContext* context)
 {
+    dil_parse__create(DIL_SYMBOL_ESCAPED, true);
+
     DilString const SET_0 = dil_string_terminated("0123456789abcdefABCDEF");
     DilString const SET_1 = dil_string_terminated("tn\\'~");
     DilString const SET_2 = dil_string_terminated("\\'~");
 
-    if (dil_parse__terminal(context, '\\')) {
-        if (dil_parse__terminal_set(context, &SET_0)) {
+    if (dil_string_prefix_element(context->string, '\\')) {
+        if (dil_string_prefix_set(context->string, &SET_0)) {
             for (size_t i = 0; i < 2 - 1; i++) {
-                if (!dil_parse__terminal_set(context, &SET_0)) {
+                if (!dil_string_prefix_set(context->string, &SET_0)) {
                     dil_parse__error_set(context, &SET_0);
-                    return true;
+                    dil_parse__return(true);
                 }
             }
-            return true;
+            dil_parse__return(true);
         }
 
-        if (dil_parse__terminal_set(context, &SET_1)) {
-            return true;
+        if (dil_string_prefix_set(context->string, &SET_1)) {
+            dil_parse__return(true);
         }
 
         dil_parse__error(context, "Unexpected character in `Escaped`!");
-        return true;
+        dil_parse__return(true);
     }
 
-    if (dil_parse__terminal_not_set(context, &SET_2)) {
-        return true;
+    if (dil_string_prefix_not_set(context->string, &SET_2)) {
+        dil_parse__return(true);
     }
 
-    return false;
+    dil_parse__return(false);
 }
 
 /* Try to parse a number. */
 bool dil_parse_number(DilParseContext* context)
 {
-    dil_parse__create(DIL_SYMBOL_NUMBER, false);
+    dil_parse__create(DIL_SYMBOL_NUMBER, true);
 
     DilString const SET_0 = dil_string_terminated("123456789");
     DilString const SET_1 = dil_string_terminated("0123456789");
 
-    if (!dil_parse__terminal_set(context, &SET_0)) {
+    if (!dil_string_prefix_set(context->string, &SET_0)) {
         dil_parse__return(false);
     }
 
     dil_parse__skip(context);
 
-    while (dil_parse__terminal_set(context, &SET_1)) {}
+    while (dil_string_prefix_set(context->string, &SET_1)) {}
 
     dil_parse__return(true);
-}
-
-/* Try to parse a as terminal set. */
-bool dil_parse_set__terminal(DilParseContext* context)
-{
-    if (!dil_parse__terminal(context, '\'')) {
-        return false;
-    }
-
-    while (dil_parse_escaped__terminal(context)) {
-        if (!dil_parse__terminal(context, '~')) {
-            continue;
-        }
-        if (!dil_parse_escaped__terminal(context)) {
-            dil_parse__error(context, "Expected `Escaped` in `Set`!");
-            return true;
-        }
-    }
-
-    if (!dil_parse__terminal(context, '\'')) {
-        dil_parse__error(context, "Expected `'` in `Set`!");
-        return true;
-    }
-
-    return true;
 }
 
 /* Try to parse a set. */
 bool dil_parse_set(DilParseContext* context)
 {
-    dil_parse__create(DIL_SYMBOL_SET, false);
-    dil_parse__return(dil_parse_set__terminal(context));
+    dil_parse__create(DIL_SYMBOL_SET, true);
+
+    if (!dil_string_prefix_element(context->string, '\'')) {
+        dil_parse__return(false);
+    }
+
+    while (dil_parse_escaped(context)) {
+        if (!dil_string_prefix_element(context->string, '~')) {
+            continue;
+        }
+        if (!dil_parse_escaped(context)) {
+            dil_parse__error(context, "Expected `Escaped` in `Set`!");
+            dil_parse__return(true);
+        }
+    }
+
+    if (!dil_string_prefix_element(context->string, '\'')) {
+        dil_parse__error(context, "Expected `'` in `Set`!");
+        dil_parse__return(true);
+    }
+
+    dil_parse__return(true);
 }
 
 /* Try to parse a not set. */
 bool dil_parse_not_set(DilParseContext* context)
 {
-    dil_parse__create(DIL_SYMBOL_NOT_SET, false);
+    dil_parse__create(DIL_SYMBOL_NOT_SET, true);
 
-    if (!dil_parse__terminal(context, '!')) {
+    if (!dil_string_prefix_element(context->string, '!')) {
         dil_parse__return(false);
     }
 
-    if (!dil_parse_set__terminal(context)) {
+    if (!dil_parse_set(context)) {
         dil_parse__error(context, "Expected `Set` in `NotSet`!");
         dil_parse__return(true);
     }
@@ -284,40 +251,40 @@ bool dil_parse_not_set(DilParseContext* context)
 /* Try to parse a string. */
 bool dil_parse_string(DilParseContext* context)
 {
-    dil_parse__create(DIL_SYMBOL_STRING, false);
+    dil_parse__create(DIL_SYMBOL_STRING, true);
 
     DilString const SET_0 = dil_string_terminated("0123456789abcdefABCDEF");
     DilString const SET_1 = dil_string_terminated("tn\\\"");
     DilString const SET_2 = dil_string_terminated("\\\"");
 
-    if (!dil_parse__terminal(context, '"')) {
+    if (!dil_string_prefix_element(context->string, '"')) {
         dil_parse__return(false);
     }
 
     while (dil_string_finite(context->string)) {
-        if (dil_parse__terminal(context, '\\')) {
-            if (dil_parse__terminal_set(context, &SET_0)) {
+        if (dil_string_prefix_element(context->string, '\\')) {
+            if (dil_string_prefix_set(context->string, &SET_0)) {
                 for (size_t i = 0; i < 2 - 1; i++) {
-                    if (!dil_parse__terminal_set(context, &SET_0)) {
+                    if (!dil_string_prefix_set(context->string, &SET_0)) {
                         dil_parse__error_set(context, &SET_0);
                         dil_parse__return(true);
                     }
                 }
                 continue;
             }
-            if (dil_parse__terminal_set(context, &SET_1)) {
+            if (dil_string_prefix_set(context->string, &SET_1)) {
                 continue;
             }
             dil_parse__error(context, "Unexpected character in `String`!");
             dil_parse__return(true);
         }
-        if (dil_parse__terminal_not_set(context, &SET_2)) {
+        if (dil_string_prefix_not_set(context->string, &SET_2)) {
             continue;
         }
         break;
     }
 
-    if (!dil_parse__terminal(context, '"')) {
+    if (!dil_string_prefix_element(context->string, '"')) {
         dil_parse__error(context, "Expected `\"` in `String`!");
         dil_parse__return(true);
     }
@@ -338,7 +305,7 @@ bool dil_parse_group(DilParseContext* context)
 {
     dil_parse__create(DIL_SYMBOL_GROUP, false);
 
-    if (!dil_parse__terminal(context, '(')) {
+    if (!dil_string_prefix_element(context->string, '(')) {
         dil_parse__return(false);
     }
 
@@ -355,7 +322,7 @@ bool dil_parse_group(DilParseContext* context)
         dil_parse__skip(context);
     }
 
-    if (!dil_parse__terminal(context, ')')) {
+    if (!dil_string_prefix_element(context->string, ')')) {
         dil_parse__error(context, "Expected `)` in `Group`!");
         dil_parse__return(true);
     }
@@ -389,7 +356,7 @@ bool dil_parse_one_or_more(DilParseContext* context)
 {
     dil_parse__create(DIL_SYMBOL_ONE_OR_MORE, false);
 
-    if (!dil_parse__terminal(context, '+')) {
+    if (!dil_string_prefix_element(context->string, '+')) {
         dil_parse__return(false);
     }
 
@@ -408,7 +375,7 @@ bool dil_parse_zero_or_more(DilParseContext* context)
 {
     dil_parse__create(DIL_SYMBOL_ZERO_OR_MORE, false);
 
-    if (!dil_parse__terminal(context, '*')) {
+    if (!dil_string_prefix_element(context->string, '*')) {
         dil_parse__return(false);
     }
 
@@ -427,7 +394,7 @@ bool dil_parse_optional(DilParseContext* context)
 {
     dil_parse__create(DIL_SYMBOL_OPTIONAL, false);
 
-    if (!dil_parse__terminal(context, '?')) {
+    if (!dil_string_prefix_element(context->string, '?')) {
         dil_parse__return(false);
     }
 
@@ -480,7 +447,7 @@ bool dil_parse_pattern(DilParseContext* context)
 
     dil_parse__skip(context);
 
-    while (dil_parse__terminal(context, '|')) {
+    while (dil_string_prefix_element(context->string, '|')) {
         dil_parse__skip(context);
 
         if (!dil_parse_justaposition(context)) {
@@ -505,7 +472,7 @@ bool dil_parse_rule(DilParseContext* context)
 
     dil_parse__skip(context);
 
-    if (!dil_parse__terminal(context, '=')) {
+    if (!dil_string_prefix_element(context->string, '=')) {
         dil_parse__error(context, "Expected `=` in `Rule`!");
         dil_parse__return(true);
     }
@@ -519,7 +486,7 @@ bool dil_parse_rule(DilParseContext* context)
 
     dil_parse__skip(context);
 
-    if (!dil_parse__terminal(context, ';')) {
+    if (!dil_string_prefix_element(context->string, ';')) {
         dil_parse__error(context, "Expected `;` in `Rule`!");
         dil_parse__return(true);
     }
@@ -534,13 +501,13 @@ bool dil_parse_terminal(DilParseContext* context)
 
     DilString const TERMINALS_0 = dil_string_terminated("terminal");
 
-    if (!dil_parse__terminal_string(context, &TERMINALS_0)) {
+    if (!dil_string_prefix_check(context->string, &TERMINALS_0)) {
         dil_parse__return(false);
     }
 
     dil_parse__skip(context);
 
-    if (!dil_parse__terminal(context, ';')) {
+    if (!dil_string_prefix_element(context->string, ';')) {
         dil_parse__error(context, "Expected `;` in `Skip`!");
         dil_parse__return(true);
     }
@@ -555,7 +522,7 @@ bool dil_parse_skip(DilParseContext* context)
 
     DilString const TERMINALS_0 = dil_string_terminated("skip");
 
-    if (!dil_parse__terminal_string(context, &TERMINALS_0)) {
+    if (!dil_string_prefix_check(context->string, &TERMINALS_0)) {
         dil_parse__return(false);
     }
 
@@ -565,7 +532,7 @@ bool dil_parse_skip(DilParseContext* context)
         dil_parse__skip(context);
     }
 
-    if (!dil_parse__terminal(context, ';')) {
+    if (!dil_string_prefix_element(context->string, ';')) {
         dil_parse__error(context, "Expected `;` in `Skip`!");
         dil_parse__return(true);
     }
@@ -580,7 +547,7 @@ bool dil_parse_start(DilParseContext* context)
 
     DilString const TERMINALS_0 = dil_string_terminated("start");
 
-    if (!dil_parse__terminal_string(context, &TERMINALS_0)) {
+    if (!dil_string_prefix_check(context->string, &TERMINALS_0)) {
         dil_parse__return(false);
     }
 
@@ -593,7 +560,7 @@ bool dil_parse_start(DilParseContext* context)
 
     dil_parse__skip(context);
 
-    if (!dil_parse__terminal(context, ';')) {
+    if (!dil_string_prefix_element(context->string, ';')) {
         dil_parse__error(context, "Expected `;` in `Start`!");
         dil_parse__return(true);
     }
@@ -608,7 +575,7 @@ bool dil_parse_output(DilParseContext* context)
 
     DilString const TERMINALS_0 = dil_string_terminated("output");
 
-    if (!dil_parse__terminal_string(context, &TERMINALS_0)) {
+    if (!dil_string_prefix_check(context->string, &TERMINALS_0)) {
         dil_parse__return(false);
     }
 
@@ -621,7 +588,7 @@ bool dil_parse_output(DilParseContext* context)
 
     dil_parse__skip(context);
 
-    if (!dil_parse__terminal(context, ';')) {
+    if (!dil_string_prefix_element(context->string, ';')) {
         dil_parse__error(context, "Expected `;` in `Output`!");
         dil_parse__return(true);
     }
