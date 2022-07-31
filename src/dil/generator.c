cdebdc2fd57fa885ffac45e658d4a449426e7cda
Cem GEÇGEL <gecgelcem@outlook.com>
2022-07-31 15:30:57 +0300

    add: Generator

---
+++ src/dil/generator.c
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2022 Cem Geçgel <gecgelcem@outlook.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include "dil/buffer.c"
+#include "dil/tree.c"
+
+void dil_generate(DilBuffer* buffer, DilTree const* tree) {}
